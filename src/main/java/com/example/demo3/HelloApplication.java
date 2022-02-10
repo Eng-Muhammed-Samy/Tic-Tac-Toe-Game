65934f9613820f47adfd482bf2d783c665177447
Muhammed Mohab <muhammed.mohab@gmail.com>
2022-02-10 20:01:47 +0200

    Game logic
    Added a fully functional game logic edited button id's and created a reset if needed

--- src/main/java/com/example/demo3/HelloApplication.java
+++ src/main/java/com/example/demo3/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("connection_failed.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),  573, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
