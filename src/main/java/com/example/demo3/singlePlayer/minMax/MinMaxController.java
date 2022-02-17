800eacff4a0ad18927be03b9182821feb3122225
yasminekamal <yassminkamal92@gmail.com>
2022-02-17 13:54:27 +0200

    add levels of min max

--- src/main/java/com/example/demo3/singlePlayer/minMax/MinMaxController.java
+++ src/main/java/com/example/demo3/singlePlayer/minMax/MinMaxController.java
@@ -1,7 +1,11 @@
 package com.example.demo3.singlePlayer.minMax;
-import com.example.demo3.User;
-import com.example.demo3.database.models.UserFunctionality;
+//import com.example.demo3.User;
+//import com.example.demo3.database.models.UserFunctionality;
+import com.example.demo3.singlePlayer.minMax.levels.EasyLevel;
+import com.example.demo3.singlePlayer.minMax.levels.HardLevel;
+import com.example.demo3.singlePlayer.minMax.levels.MediumLevel;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,7 +16,7 @@ import java.util.Random;
 import java.util.ResourceBundle;
 
 public class MinMaxController   implements Initializable {
-    UserFunctionality bd = new UserFunctionality();
+//    UserFunctionality bd = new UserFunctionality();
 
     @FXML
     protected Label textLabel;
@@ -43,28 +47,14 @@ public class MinMaxController   implements Initializable {
 
     @FXML
     private Button button8;
-
     @FXML
-    protected void resetGame(){
-        try {
-            bd.insertUser(new User("ayman","ayman@gamil.com", "123456",1));
-//            bd.selectAllUsers();
-//            bd.selectUserByEmail("ahmed@gamil.com");
-//            bd.selectUserByStatus(0);
-//            bd.updateStatusByEmail(1,"ahmed@gamil.com");
-//            bd.selectUserByEmail("ahmed@gamil.com");
-        }catch(Exception ex){
-
-            ex.getMessage();
-        }
-
-    }
+    private Button Next_Round;
 
     int moveNum;
     BasicForGame.Move nextMove;
     int flag = 0;
     Random ran;
-
+    Button board[][]=new Button[3][3];
     void scoreScreen(Button[][] board) {
 
         int result = BasicForGame.evaluate(board);
@@ -80,66 +70,197 @@ public class MinMaxController   implements Initializable {
             textLabel.setText("Draw");
 
         }
+        Next_Round.setDisable(false);
     }
+
 
     void changeFlag(int num) {
         flag = num;
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Button board[][] = {{button0, button1, button2}, {button3, button4, button5}, {button6, button7, button8}};
+
+       board[0][0] =button0;
+        board[0][1]=button1;
+        board[0][2]=button2;
+        board[1][0]=button3;
+        board[1][1]=button4;
+        board[1][2]=button5;
+        board[2][0]=button6;
+        board[2][1]=button7;
+        board[2][2]=button8;
+
         ran = new Random();
         moveNum = flag = 0;
+        Next_Round.setDisable(true);
         setActionOnBtn(board);
-    }
 
-    private void setActionOnBtn(Button[][] board) {
+    }
+   @FXML
+    protected void resetGame(){
+
         for (Button[] btns : board) {
             for (Button btn : btns) {
+              this.resetButtons(btn);
 
-                btn.setOnMouseClicked(mouseEvent -> {
-                    if (flag == 0) {
-                        btn.setText("O");
-                        btn.setMouseTransparent(true);
-                        if (moveNum + 1 < 10) {
-                            changeFlag(1);
-                            nextMove = MinMax.findNextMove(board);
-                            Thread t = new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        Thread.sleep(1000);
-                                    } catch (InterruptedException e) {
 
-                                    }
-                                    Platform.runLater(() -> {
-                                        if (moveNum < 9) {
-                                            board[nextMove.row][nextMove.col].setText("X");
-                                            board[nextMove.row][nextMove.col].setMouseTransparent(true);
-                                        }
-                                        changeFlag(0);
-                                    });
-                                    moveNum += 2;
-                                    if (moveNum >= 5) {
-
-                                        Platform.runLater(() -> {
-                                            scoreScreen(board);
-                                        });
-                                    }
-
-                                }
-                            });
-
-                            t.start();
-
-                        }
-
-                    }
-                });
             }
-
-
         }
+       board[0][0] =button0;
+       board[0][1]=button1;
+       board[0][2]=button2;
+       board[1][0]=button3;
+       board[1][1]=button4;
+       board[1][2]=button5;
+       board[2][0]=button6;
+       board[2][1]=button7;
+       board[2][2]=button8;
+       ran = new Random();
+       moveNum = flag = 0;
+       Next_Round.setDisable(true);
+       setActionOnBtn(board);
 
     }
-}
+
+    public void resetButtons(Button button){//Function to reset the game board could be used for "next round"
+            button.setText("");
+    }
+    private void setActionOnBtn(Button[][] board) {
+
+        for (Button[] btns : board) {
+            for (Button btn : btns) {
+                switch ("Hard") {
+
+                    case "Hard":
+                        btn.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
+                            if (flag == 0) {
+                                btn.setText("O");
+                                btn.setMouseTransparent(true);
+                                if (moveNum + 1 < 10) {
+                                    changeFlag(1);
+                                    nextMove = HardLevel.findNextMove(board);
+                                    Thread t = new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            try {
+                                                Thread.sleep(1000);
+                                            } catch (InterruptedException e) {
+
+                                            }
+                                            Platform.runLater(() -> {
+                                                if (moveNum < 9) {
+                                                    board[nextMove.row][nextMove.col].setText("X");
+                                                    board[nextMove.row][nextMove.col].setMouseTransparent(true);
+                                                }
+                                                changeFlag(0);
+                                            });
+                                            moveNum += 2;
+                                            if (moveNum >= 5) {
+
+                                                Platform.runLater(() -> {
+                                                    scoreScreen(board);
+
+                                                });
+                                                Next_Round.setDisable(false);
+                                            }
+
+                                        }
+                                    });
+
+                                    t.start();
+
+                                }
+
+                            }
+                        });
+                        break;
+                    case "Easy":
+                        btn.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
+                            if (flag == 0) {
+                                btn.setText("O");
+                                btn.setMouseTransparent(true);
+                                if (moveNum + 1 < 10) {
+                                    changeFlag(1);
+                                    nextMove = EasyLevel.findNextMove(board);
+                                    Thread t = new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            try {
+                                                Thread.sleep(1000);
+                                            } catch (InterruptedException e) {
+
+                                            }
+                                            Platform.runLater(() -> {
+                                                if (moveNum < 9) {
+                                                    board[nextMove.row][nextMove.col].setText("X");
+                                                    board[nextMove.row][nextMove.col].setMouseTransparent(true);
+                                                }
+                                                changeFlag(0);
+                                            });
+                                            moveNum += 2;
+                                            if (moveNum >= 5) {
+
+                                                Platform.runLater(() -> {
+                                                    scoreScreen(board);
+
+                                                });
+                                                Next_Round.setDisable(false);
+                                            }
+
+                                        }
+                                    });
+
+                                    t.start();
+
+                                }
+
+                            }
+                        });
+                        break;
+                    case "Medium":
+                        btn.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
+                            if (flag == 0) {
+                                btn.setText("O");
+                                btn.setMouseTransparent(true);
+                                if (moveNum + 1 < 10) {
+                                    changeFlag(1);
+                                    nextMove = MediumLevel.findNextMove(board);
+                                    Thread t = new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            try {
+                                                Thread.sleep(1000);
+                                            } catch (InterruptedException e) {
+
+                                            }
+                                            Platform.runLater(() -> {
+                                                if (moveNum < 9) {
+                                                    board[nextMove.row][nextMove.col].setText("X");
+                                                    board[nextMove.row][nextMove.col].setMouseTransparent(true);
+                                                }
+                                                changeFlag(0);
+                                            });
+                                            moveNum += 2;
+                                            if (moveNum >= 5) {
+
+                                                Platform.runLater(() -> {
+                                                    scoreScreen(board);
+                                                });
+                                                Next_Round.setDisable(false);
+                                            }
+
+                                        }
+                                    });
+
+                                    t.start();
+
+                                }
+
+                            }
+                        });
+                        break;
+                }
+            }
+
+        }
+}}
